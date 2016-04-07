d85cb29ae4e4df79843fc0ddc4959c51b2d5c0c2
Michael Herman <hermanmu@gmail.com>
2016-04-06 20:42:15 -0600

    refactor

--- scripts/app.js
+++ scripts/app.js
@@ -8,17 +8,37 @@ app.controller('cashController', ['$scope', function($scope) {
   $scope.subTotal = 0;
   $scope.total = 0;
 
-  $scope.itemList = {
-    pizza: 0,
-    salad: 0,
-    burger: 0,
-    cheeseburger: 0
-  };
+  $scope.itemList = [
+    {
+      name: 'pizza',
+      price: 12.99,
+      amount: 0
+    },
+    {
+      name: 'salad',
+      price: 5.99,
+      amount: 0
+    },
+    {
+      name: 'burger',
+      price: 12.99,
+      amount: 0
+    },
+    {
+      name: 'cheeseburger',
+      price: 12.99,
+      amount: 0
+    }
+  ];
 
   // add item
-  $scope.addItem = function(item, price) {
-    $scope.itemList[item] += 1;
-    $scope.subTotal += parseFloat(price);
+  $scope.addItem = function(item) {
+    $scope.itemList.forEach(function(el){
+      if(el.name === item) {
+        el.amount +=1;
+        $scope.subTotal += parseFloat(el.price);
+      }
+    });
     $scope.tax = taxRate * $scope.subTotal;
     $scope.total = $scope.subTotal + $scope.tax;
   };
